7d40eaaf4532bad7158137179627974760dea0e2
oboro <my.ancestor.skanderbeg@gmail.com>
2024-01-29 03:10:15 +0900

    react組み込み途中

--- my-blog-app/src/App.jsx
+++ my-blog-app/src/App.jsx
@@ -1,13 +1,13 @@
 import React, { useEffect, useState } from 'react'
 import axios from "axios"
-import Blog from '/Applications/XAMPP/htdocs/wordpress/wp-content/themes/template/my-blog-app/components/blog';
+import Blog from './components/Blog';
 
 export default function App() {
   const [posts, setPosts] = useState([]);
 
   const fetchPosts = () => {
     axios
-      .get("http://bamboo-brush-art.com/wp-json/wp/v2/posts?_embed")
+      .get("http://bamboo-brush-art.com/wp-json/wp/v2/posts")
       .then((res) => {
         setPosts(res.data);
       });
